8a7d9ee02dd45696840af7ec6d62956112f34745
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2026-01-07 14:47:52 -0500

    Update customer_routes.py

--- WS24WebService/src/routes/customer_routes.py
+++ WS24WebService/src/routes/customer_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from src.services.customer_service import CustomerService
 
 customer_bp = Blueprint('customer', __name__)
@@ -9,3 +9,10 @@ customer_service = CustomerService()
 def index():
     customers = customer_service.get_all_customers()
     return render_template('index.html', customers=customers)
+
+
+@customer_bp.route('/customers', methods=['GET'])
+def get_customers():
+    customers = customer_service.get_all_customers()
+    customers_list = [customer.to_dict() for customer in customers]
+    return jsonify(customers_list), 200
